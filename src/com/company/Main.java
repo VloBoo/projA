c09acc3f48feee11048b912ac65654e55ec9e89a
VloBoo <velik2004@gmail.com>
2020-02-01 15:23:03 +0300

    master comm

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            String url = "jdbc:mysql://localhost/glazz?serverTimezone=Europe/Minsk&useSSL=false";
+            String username = "root";
+            String password = "";
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = DriverManager.getConnection(url, username, password)){
+
+                System.out.println("Connection to Store DB succesfull!");
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+            System.out.println(ex);
+        }
+    }
+}
+
